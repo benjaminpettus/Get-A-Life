8222114028e5f5c0e1fe3eed79b85528628f12cb
Gail Matsushima <gail.matsushima@gmail.com>
2016-03-24 21:02:50 -1000

    got basic routes up for login and create event

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 //CHECK: need bodyParser.json also?
 
-app.get('/maps', function(req, res) {
-  console.log('Im getting');
-  res.send('I\'m getting');
+// app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.get('/login', function (req, res){
+  res.redirect('/templates/login.html');
 });
 
-app.post('/events', function(req, res) {
-  console.log('req.body', req.body);
-  res.send('I\'m posting');
+app.post('/events', function(req, res){
+  console.log(req.body);
+  res.redirect('/templates/create-event.html');
 });
 
 app.listen(3000, function() {
