cdedbe105fc5f5676ae38f12907186a3e5411726
Gail Matsushima <gail.matsushima@gmail.com>
2016-03-24 17:53:38 -1000

    adding get and post routes

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express =  require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:false}));
+//CHECK: need bodyParser.json also?
+
+app.get('/', function(req, res) {
+  res.send('I\'m getting');
+});
+
+app.post('/events', function(req, res) {
+  console.log('req.body', req.body);
+  res.send('I\'m posting');
+});
 
 app.listen(3000, function() {
   console.log('server is connected');
