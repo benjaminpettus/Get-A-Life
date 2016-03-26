7c9ec815db01ec7e6c3ca80961ec63794d83cdeb
Gail Matsushima <gail.matsushima@gmail.com>
2016-03-25 19:33:41 -1000

    connected to database and postman

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
-var express =  require('express');
+var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/Get_A_Life');
+
+var eventSchema = mongoose.Schema({
+  title: String,
+  created_by: String,
+  description: String,
+  latitude: Number,
+  longitude: Number,
+  start_time: Date,
+  posts: Array
+});
+
+var Event = mongoose.model('Event', eventSchema);
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+var User = mongoose.model('User', userSchema);
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 //CHECK: need bodyParser.json also?
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/login', function (req, res){
-  res.redirect('/templates/login.html');
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/events', function(req, res) {
+  return Event.findAll({}).then(function(events){
+    res.render('index');
+  });
 });
 
+app.get('/newEvent', function(req, res){
+  res.render('create-event');
+});
+
+// app.get('/login', function(req, res){
+//   res.redirect('/templates/login.html');
+// });
+
 app.post('/events', function(req, res){
-  console.log(req.body);
-  res.redirect('/templates/create-event.html');
+  //TODO: ajax request POST for Ben's setContent
+  var newEvent = new Event({
+    title: req.body.title,
+    created_by: req.body.created_by,
+    description: req.body.description,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    start_time: req.body.start_time,
+    posts: req.body.posts
+  });
+  newEvent.save(function(){
+    res.send('Done');
+  });
 });
 
 app.listen(3000, function() {
