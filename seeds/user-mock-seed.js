2c4476370af16dbaff004ad8008bec4ab7a2f3ce
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-25 18:40:00 -1000

    created user seeder

---
+++ seeds/user-mock-seed.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+var faker = require('faker');
+
+mongoose.connect('mongodb://localhost/Get_A_Life');
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+var User = mongoose.model('User', userSchema);
+
+var users= [];
+
+for(var i=0; i < 5; i++) {
+  users.push({
+    username: faker.internet.userName(),
+    password: 'testUser'
+  });
+}
+
+User.create(users, function(err, savedUsers) {
+  if (err) {
+    throw err;
+  }
+  console.log(savedUsers);
+});
